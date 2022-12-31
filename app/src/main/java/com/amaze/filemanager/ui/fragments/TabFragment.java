0f0d9960e1e5f9523aa14bbe1f68d141871caa10
Raymond Lai <airwave209gt@gmail.com>
2022-12-31 22:56:52 +0800

    Changes per PR feedback
    - TabFragment Fix ScreenSlidePagerAdapter creation with FragmentManager and TabFragment's own lifecycle state
    - Added MainFragment.onSaveInstanceState()

--- app/src/main/java/com/amaze/filemanager/ui/fragments/TabFragment.java
+++ app/src/main/java/com/amaze/filemanager/ui/fragments/TabFragment.java
@@ -67,6 +67,7 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
 import androidx.preference.PreferenceManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
@@ -133,7 +134,7 @@ public class TabFragment extends Fragment {
     requireMainActivity().supportInvalidateOptionsMenu();
     viewPager.registerOnPageChangeCallback(new OnPageChangeCallbackImpl());
 
-    sectionsPagerAdapter = new ScreenSlidePagerAdapter(this);
+    sectionsPagerAdapter = new ScreenSlidePagerAdapter(fragmentManager, getLifecycle());
     if (savedInstanceState == null) {
       int lastOpenTab = sharedPrefs.getInt(PREFERENCE_CURRENT_TAB, DEFAULT_CURRENT_TAB);
       MainActivity.currentTab = lastOpenTab;
@@ -159,7 +160,7 @@ public class TabFragment extends Fragment {
         LOG.warn("failed to clear fragments", e);
       }
 
-      sectionsPagerAdapter = new ScreenSlidePagerAdapter(this);
+      sectionsPagerAdapter = new ScreenSlidePagerAdapter(fragmentManager, getLifecycle());
 
       viewPager.setAdapter(sectionsPagerAdapter);
       int pos1 = savedInstanceState.getInt(KEY_POSITION, 0);
@@ -312,8 +313,8 @@ public class TabFragment extends Fragment {
   }
 
   private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
-    public ScreenSlidePagerAdapter(Fragment fm) {
-      super(fm);
+    public ScreenSlidePagerAdapter(FragmentManager fm, Lifecycle lifecycle) {
+      super(fm, lifecycle);
     }
 
     @Override
